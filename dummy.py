887bd1bf2408fc8a4af43f6161d01fbda2bfc3f2
Denis Ermakov <136415369+DatusRerum@users.noreply.github.com>
2023-06-28 11:05:00 -0700

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,1 +1,14 @@
-print("This is a dummy Python file")
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def home():
+    return app.send_static_file('index.html')
+
+@app.route('/<path:path>')
+def static_file(path):
+    return app.send_static_file(path)
+
+if __name__ == "__main__":
+app.run()
